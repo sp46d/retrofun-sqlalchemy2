f7628a0ff96812c9fd3c3d83e671fe09a453737d
Sanghyuk Park <sanghyuk.park85@gmail.com>
2024-08-09 22:49:16 -0500

    make manufacturer column a separate table from products table

--- retrofun/import_data.py
+++ retrofun/import_data.py
@@ -1,15 +1,18 @@
 import csv
 import os
 from retrofun.db import Model, engine, Session
-from retrofun.models import Product
+from retrofun.models import Product, Manufacturer
+
 
 products_path = os.path.join(os.path.dirname(__file__), 'data', 'products_with_id.csv')
+manufacturers_path = os.path.join(os.path.dirname(__file__), 'data', 'manufacturers.csv')
 
 def main():
     with Session() as session:
         with session.begin():
             Model.metadata.drop_all(engine)
             Model.metadata.create_all(engine)
+            # Load products_with_id.csv and add it to database
             with open(products_path) as f:
                 products = csv.DictReader(f)
                 for row in products:
@@ -17,6 +20,12 @@ def main():
                     row['manufacturer_id'] = int(row['manufacturer_id'])
                     product = Product(**row)
                     session.add(product)
+            # Load manufacturer.csv and add it to database
+            with open(manufacturers_path) as f:
+                manufacturers = csv.DictReader(f)
+                for row in manufacturers:
+                    manufacturer = Manufacturer(**row)
+                    session.add(manufacturer)
                     
 
 if __name__ == '__main__':
--- retrofun/models.py
+++ retrofun/models.py
@@ -1,17 +1,32 @@
-from sqlalchemy import String
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from retrofun.db import Model
+from typing import Optional
 
 
 class Product(Model):
     __tablename__ = 'products'
     
     id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(String(64))
-    manufacturer: Mapped[str] = mapped_column(String(64))
-    year: Mapped[int]
-    country: Mapped[str] = mapped_column(String(32))
-    cpu: Mapped[str] = mapped_column(String(32))
+    name: Mapped[str] = mapped_column(String(64), index=True, unique=True)
+    manufacturer_id: Mapped[int] = mapped_column(ForeignKey('manufacturers.id'))
+    year: Mapped[int] = mapped_column(index=True)
+    country: Mapped[Optional[str]] = mapped_column(String(32))
+    cpu: Mapped[Optional[str]] = mapped_column(String(32))
+    
+    manufacturer: Mapped['Manufacturer'] = relationship(back_populates='product')
     
     def __repr__(self):
         return f'Product({self.id}, "{self.name}")'
+    
+    
+class Manufacturer(Model):
+    __tablename__ = 'manufacturers'
+    
+    id: Mapped[int] = mapped_column(primary_key=True)
+    manufacturer: Mapped[str] = mapped_column(String(64), unique=True, index=True)
+    
+    product: Mapped[list['Product']] = relationship(back_populates='manufacturer')
+    
+    def __repr__(self) -> str:
+        return f'Manufacturer({self.id}, "{self.manufacturer}")'
