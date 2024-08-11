e04532808efc02301753e5be98446d85786297e3
Sanghyuk Park <sanghyuk.park85@gmail.com>
2024-08-10 22:17:54 -0500

    rewrite import_products logic using sqlalchemy relationship construct

--- retrofun/import_products.py
+++ retrofun/import_products.py
@@ -1,22 +1,31 @@
 import csv
 import os
 from retrofun.db import Model, engine, Session
-from retrofun.models import Product
+from retrofun.models import Product, Manufacturer
 
-products_path = os.path.join(os.path.dirname(__file__), 'data', 'products_with_id.csv')
+products_path = os.path.join(os.path.dirname(__file__), 'data', 'products.csv')
+
 
 def main():
+    Model.metadata.drop_all(engine)
+    Model.metadata.create_all(engine)
+
     with Session() as session:
         with session.begin():
-            Model.metadata.drop_all(engine)
-            Model.metadata.create_all(engine)
             with open(products_path) as f:
-                products = csv.DictReader(f)
-                for row in products:
+                reader = csv.DictReader(f)
+                all_manufacturers = {}
+                
+                for row in reader:
                     row['year'] = int(row['year'])
-                    row['manufacturer_id'] = int(row['manufacturer_id'])
-                    product = Product(**row)
-                    session.add(product)
+                    manufacturer = row.pop('manufacturer')
+                    p = Product(**row)
+                    
+                    if manufacturer not in all_manufacturers:
+                        m = Manufacturer(name=manufacturer)
+                        session.add(m)
+                        all_manufacturers[manufacturer] = m
+                    all_manufacturers[manufacturer].products.append(p)
                     
 
 if __name__ == '__main__':
--- retrofun/models.py
+++ retrofun/models.py
@@ -1,9 +1,10 @@
 from sqlalchemy import String, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from retrofun.db import Model
-from typing import Optional
+from typing import Optional, List
 
 
+# many side of one-to-many relationship
 class Product(Model):
     __tablename__ = 'products'
     
@@ -14,19 +15,20 @@ class Product(Model):
     country: Mapped[Optional[str]] = mapped_column(String(32))
     cpu: Mapped[Optional[str]] = mapped_column(String(32))
     
-    manufacturer: Mapped['Manufacturer'] = relationship(back_populates='product')
+    manufacturer: Mapped['Manufacturer'] = relationship(back_populates='products')
     
     def __repr__(self):
         return f'Product({self.id}, "{self.name}")'
     
-    
+
+# one side of one-to-many relationship
 class Manufacturer(Model):
     __tablename__ = 'manufacturers'
     
     id: Mapped[int] = mapped_column(primary_key=True)
-    manufacturer: Mapped[str] = mapped_column(String(64), unique=True, index=True)
+    name: Mapped[str] = mapped_column(String(64), unique=True, index=True)
     
-    product: Mapped[list['Product']] = relationship(back_populates='manufacturer')
+    products: Mapped[List['Product']] = relationship(back_populates='manufacturer')
     
     def __repr__(self) -> str:
-        return f'Manufacturer({self.id}, "{self.manufacturer}")'
+        return f'Manufacturer({self.id}, "{self.name}")'
