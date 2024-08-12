91bd64dd9f4bcc1aca71647e935c840efc47127c
Sanghyuk Park <sanghyuk.park85@gmail.com>
2024-08-11 22:40:54 -0500

    make country column a separate table from products table

--- retrofun/import_products.py
+++ retrofun/import_products.py
@@ -1,7 +1,7 @@
 import csv
 import os
 from retrofun.db import Model, engine, Session
-from retrofun.models import Product, Manufacturer
+from retrofun.models import Product, Manufacturer, Country
 
 products_path = os.path.join(os.path.dirname(__file__), 'data', 'products.csv')
 
@@ -14,11 +14,13 @@ def main():
         with session.begin():
             with open(products_path) as f:
                 reader = csv.DictReader(f)
-                all_manufacturers = {}
+                all_manufacturers: dict[str, Manufacturer] = {}
+                all_countries: dict[str, Country] = {}
                 
                 for row in reader:
                     row['year'] = int(row['year'])
                     manufacturer = row.pop('manufacturer')
+                    countries = row.pop('country').split('/')
                     p = Product(**row)
                     
                     if manufacturer not in all_manufacturers:
@@ -27,6 +29,12 @@ def main():
                         all_manufacturers[manufacturer] = m
                     all_manufacturers[manufacturer].products.append(p)
                     
+                    for country in countries:
+                        if country not in all_countries:
+                            c = Country(name=country)
+                            session.add(c)
+                            all_countries[country] = c
+                        all_countries[country].products.append(p)
 
 if __name__ == '__main__':
     main()
--- retrofun/models.py
+++ retrofun/models.py
@@ -1,7 +1,16 @@
-from sqlalchemy import String, ForeignKey
+from sqlalchemy import String, Integer, ForeignKey, Table, Column
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from retrofun.db import Model
 from typing import Optional, List
+
+
+# Join table between products and countries (many-to-many relationships)
+ProductCountry = Table(
+    'products_countries',
+    Model.metadata,
+    Column('product_id', Integer, ForeignKey('products.id'), nullable=False, primary_key=True),
+    Column('country_id', Integer, ForeignKey('countries.id'), nullable=False, primary_key=True)
+)
 
 
 # many side of one-to-many relationship
@@ -12,10 +21,10 @@ class Product(Model):
     name: Mapped[str] = mapped_column(String(64), index=True, unique=True)
     manufacturer_id: Mapped[Optional[int]] = mapped_column(ForeignKey('manufacturers.id'))
     year: Mapped[int] = mapped_column(index=True)
-    country: Mapped[Optional[str]] = mapped_column(String(32))
     cpu: Mapped[Optional[str]] = mapped_column(String(32))
     
     manufacturer: Mapped['Manufacturer'] = relationship(back_populates='products')
+    countries: Mapped[List['Country']] = relationship(secondary=ProductCountry, back_populates='products')
     
     def __repr__(self):
         return f'Product({self.id}, "{self.name}")'
@@ -32,3 +41,16 @@ class Manufacturer(Model):
     
     def __repr__(self) -> str:
         return f'Manufacturer({self.id}, "{self.name}")'
+    
+
+# many-to-many relationship with Product
+class Country(Model):
+    __tablename__ = 'countries'
+    
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(32), unique=True, index=True)
+    
+    products: Mapped[List['Product']] = relationship(secondary=ProductCountry, back_populates='countries')
+    
+    def __repr__(self) -> str:
+        return f'Country({self.id}, "{self.name})'
